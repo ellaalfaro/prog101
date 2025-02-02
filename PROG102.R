4dfe0e818d527e2884de5a492be4d2e76a3984f2
ellaalfaro <ella779@ucsb.edu>
2025-02-02 11:39:13 -0800

    Complete PROG102.R

---
+++ PROG102.R
@@ -0,0 +1,18 @@
+hottest_idx <- which.max(kefj_temperature)
+hottest_time <- kefj_datetime[hottest_idx]
+hottest_site <- kefj_site[hottest_idx]
+hotday_start <- as.POSIXct("2018-07-03 00:00:00", tz = "Etc/GMT+8")
+hotday_end <- as.POSIXct("2018-07-03 23:59:59", tz = "Etc/GMT+8")
+hotday_idx <- kefj_site == hottest_site &
+  kefj_datetime >= hotday_start &
+  kefj_datetime <= hotday_end
+hotday_datetime <- kefj_datetime[hotday_idx]
+hotday_temperature <- kefj_temperature[hotday_idx]
+hotday_exposure <- kefj_exposure[hotday_idx]
+plot_kefj(hotday_datetime, hotday_temperature, hotday_exposure)
+
+
+??? <- ???(???) {
+  ???
+    return(???)
+}
